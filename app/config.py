26d430b721864295fa5e22d7a6cfea14b6dc0408
Minkyu Jung <hestati63@gmail.com>
2016-06-09 19:57:06 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,9 @@
 #-*- coding: utf-8 -*-
-SECRET = '\x121\x11\xccRBv\x1e\xa1\xa7\xf36\xa5\x17Q\xfc \xc3\xf79\xa7\xa7\xa6Re\x93\xb9\x94<\xc7Vq'
-dbfile = './tmp.db'
+SECRET = '\x121\x11\xccRBv\x1e\xa1\xa7\xf36\xa5\x17Q\xfc \xc3\xf79\xa7\xa7\xa6Re\x93\xb9\x94<\xc7Vq' # should be change
+dbfile = './tmp.db' # db for connection
 
-category = ["Pwnable", "Reversing", "Webhacking", "Forensic", "Coding"]
-score_max = 600
-user_num = 40
+category = ["Pwnable", "Reversing", "Webhacking", "Forensic", "Coding"] # exist categories
+score_max = 600 # score when only 1 player solves
 
-registkey = "a"
-admin_registkey = "b"
+registkey = "a" # registration key for normal user
+admin_registkey = "b" # registration key for admin
